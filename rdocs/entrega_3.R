db9e52c064f2eed8fe18e721f553156b6ba5080a
LorenaGarciaFerracin <lo19rena05@gmail.com>
2025-10-27 17:56:15 -0300

    o

--- rdocs/entrega_3.R
+++ rdocs/entrega_3.R
@@ -74,7 +74,7 @@ box<-ggplot(bancos3) +
   geom_boxplot(fill = c("#A11D21"), width = 0.5) +
   stat_summary(
     fun = "mean", geom = "point", shape = 23, size = 3, fill = "white") +
-  labs(x = "Nome da Loja de Âmbar Seco", y = "Idade (Anos)") +
+  labs(x = "Nome da Loja", y = "Idade (Anos)") +
   theme_estat()
 ggsave("box_bi.pdf", width = 158, height = 93, units = "mm")
 box
